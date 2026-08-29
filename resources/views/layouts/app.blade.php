c2af47539152bb50f41cdb3617b78c82a2eeaaeb
Jay-ar Cristobal <jayarcristobal@gmail.com>
2026-08-29 23:33:05 +0800

    Prevent CRM search engine indexing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
     @php
         $headBrand = auth()->user()?->brand;
         $headBrandName = $headBrand?->imprint_name ?? 'CreatiVision Outsourcing';
